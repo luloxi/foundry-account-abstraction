1c7f9324a3349e5e6c55080d26599b309e8d7174
Lulox <lulox.eth@protonmail.com>
2024-07-01 01:30:20 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# About
+
+1. Create a basic AA on Ethereum
+2. Create a basic AA on zkSync
+3. Deploy, and send a userOp / transaction through them
+   1. Not going to send an AA tx to Ethereum
+   2. But will send an AA tx to zkSync
+
+## Account abstraction libraries:
+
+`forge install eth-infinitism/account-abstraction@v0.7.0 --no-commit`
+
+`forge install openzeppelin/openzeppelin-contracts@v5.0.2 --no-commit`
---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+contract MinimalAccount is IAccount, Ownable {
+    IEntryPoint private immutable i_entryPoint;
+    // Nonce uniqueness is managed by the EntryPoint itself, so we shouldn't do anything
+    // uint256 nonce;
+
+    error MinimalAccount__NotFromEntryPoint();
+
+    modifier requireFromEntryPoint() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+
+    // A signature is valid if it's the contract owner
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        requireFromEntryPoint
+        returns (uint256 validationData)
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce();
+        // This is what will pay for the gas, here we could specify a paymaster maybe?
+        _payPrefund(missingAccountFunds);
+    }
+
+    // EIP-191 version of the signed hash
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            // Maybe this this is not needed but the compiler likes it vvv
+            require(success, "MinimalAccount: PayPrefund failed");
+        }
+    }
+
+    /*///////////////////////////////////////////////
+    ////////////////  GETTERS ///////////////////////
+    ///////////////////////////////////////////////*/
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
+    }
+}
